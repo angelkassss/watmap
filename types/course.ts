6e8fd04cad85cdde3e8c1e3d21b2b800d52ca415
Anjile <anjilekasturiak@gmail.com>
2026-05-03 17:31:10 -0400

    course's data types have been added

---
+++ types/course.ts
@@ -0,0 +1,39 @@
+// what a course is allowed to look like
+
+export type CourseCategory =
+    | "core"
+    | "technical-elective"
+    | "complementary-studies"
+    | "nautural-science"
+    | "pd"
+    | "coop"
+    | "milestone"
+    | "capstone";
+
+export type FocusArea =
+    | "software"
+    | "hardware"
+    | "systems"
+    | "embedded"
+    | "ai-ml"
+    | "data-science"
+    | "cybersecurity"
+    | "theory"
+    | "circuits"
+    | "communications"
+    | "control-systems"
+    | "signal-processing"
+    | "computer-architecture"
+    | "power-control";
+
+export type Course = {
+    id: string;
+    code: string;
+    title: string;
+    term: string;
+    category: CourseCategory;
+    description?: string;
+    Prerequisites?: string[];
+    FocusArea?: FocusArea[];
+    units?: number;
+};
